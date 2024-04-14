a37455d3dfd81b7cf487b1009d562e4dd4d3a86e
danX <danildryzov@gmail.com>
2024-04-14 14:17:17 +0200

    replaced where with sqlrestriction

--- src/main/java/mate/academy/bookstore/model/Book.java
+++ src/main/java/mate/academy/bookstore/model/Book.java
@@ -11,14 +11,14 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.Where;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Getter
 @Setter
 @ToString
-@SQLDelete(sql = "UPDATE book SET is_deleted = true WHERE id=?")
-@Where(clause = "is_deleted=false")
+@SQLDelete(sql = "UPDATE book SET is_deleted = TRUE WHERE id=?")
+@SQLRestriction("is_deleted=FALSE")
 @Table(name = "book")
 public class Book {
     @Id
