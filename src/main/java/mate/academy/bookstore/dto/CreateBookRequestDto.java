13259d991b064f90e040fff3394b1fbf653a972d
Daniil Dryzhov <danildryzov@gmail.com>
2024-05-31 12:03:00 +0200

    use record in BookDto and CreateBookRequestDto, added validation for id in controller, changed size value in some validation fields according to db

--- src/main/java/mate/academy/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/bookstore/dto/CreateBookRequestDto.java
@@ -5,34 +5,29 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
 
-@Getter
-@Setter
-@ToString
-public class CreateBookRequestDto {
-    @NotBlank(message = "Title cannot be blank")
-    @Size(max = 255, message = "Title can be at most 255 characters long")
-    private String title;
+public record CreateBookRequestDto(
+        @NotBlank(message = "Title cannot be blank")
+        @Size(max = 255, message = "Title can be at most 255 characters long")
+        String title,
 
-    @NotBlank(message = "Author cannot be blank")
-    @Size(max = 255, message = "Author can be at most 255 characters long")
-    private String author;
+        @NotBlank(message = "Author cannot be blank")
+        @Size(max = 255, message = "Author can be at most 255 characters long")
+        String author,
 
-    @NotBlank(message = "ISBN cannot be blank")
-    @Size(max = 13, message = "ISBN can be at most 13 characters long")
-    private String isbn;
+        @NotBlank(message = "ISBN cannot be blank")
+        @Size(max = 255, message = "ISBN can be at most 255 characters long")
+        String isbn,
 
-    @NotNull(message = "Price cannot be null")
-    @Positive(message = "Price must be greater than zero")
-    private BigDecimal price;
+        @NotNull(message = "Price cannot be null")
+        @Positive(message = "Price must be greater than zero")
+        BigDecimal price,
 
-    @Size(max = 1000, message = "Description can be at most 1000 characters long")
-    private String description;
+        @Size(max = 255, message = "Description can be at most 255 characters long")
+        String description,
 
-    @NotBlank(message = "Cover image cannot be blank")
-    @Size(max = 255, message = "Cover Image URL can be at most 255 characters long")
-    private String coverImage;
+        @NotBlank(message = "Cover image cannot be blank")
+        @Size(max = 255, message = "Cover Image URL can be at most 255 characters long")
+        String coverImage
+) {
 }
