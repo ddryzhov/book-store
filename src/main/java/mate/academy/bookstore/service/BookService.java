8cf1b43086e25200653f80d76345ab2f4c017242
Daniil Dryzhov <danildryzov@gmail.com>
2024-05-31 19:40:20 +0200

    added Pagination, Sorting, and Swagger

--- src/main/java/mate/academy/bookstore/service/BookService.java
+++ src/main/java/mate/academy/bookstore/service/BookService.java
@@ -4,11 +4,12 @@ import java.util.List;
 import mate.academy.bookstore.dto.BookDto;
 import mate.academy.bookstore.dto.BookSearchParameters;
 import mate.academy.bookstore.dto.CreateBookRequestDto;
+import org.springframework.data.domain.Pageable;
 
 public interface BookService {
     BookDto save(CreateBookRequestDto requestDto);
 
-    List<BookDto> findAll();
+    List<BookDto> findAll(Pageable pageable);
 
     BookDto findById(Long id);
 
@@ -16,5 +17,5 @@ public interface BookService {
 
     BookDto update(Long id, BookDto bookDto);
 
-    List<BookDto> search(BookSearchParameters params);
+    List<BookDto> search(BookSearchParameters params, Pageable pageable);
 }
