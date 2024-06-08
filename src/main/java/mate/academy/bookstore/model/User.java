74a8961d4bb73ebdbd1c37cf9e72834aa80c6c1b
Daniil Dryzhov <danildryzov@gmail.com>
2024-06-08 09:47:43 +0200

    email is unique now

--- src/main/java/mate/academy/bookstore/model/User.java
+++ src/main/java/mate/academy/bookstore/model/User.java
@@ -1,5 +1,6 @@
 package mate.academy.bookstore.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,6 +21,7 @@ public class User {
     private Long id;
 
     @NotBlank
+    @Column(unique = true)
     private String email;
 
     @NotBlank
